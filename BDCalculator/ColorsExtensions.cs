327ab80f4fc251d6e2115aff7d9a0167fa11ec6f
Dmitro <klopovdmitriy@gmail.com>
2020-10-18 08:39:21 +0500

    delete exra files and cache

--- BDCalculator/ColorsExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Media;
-
-namespace BDCalculator
-{
-    public static class ColorsExtensions
-    {
-        public static bool IsRed(this Color color) => color == Colors.Red;
-    }
-}
--- BDCalculator/Models/EnergyCircle.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace BDCalculator.Models
-{
-    public static class EnergyCircle
-    {
-        public static List<string> EntryPoints => new List<string> {"TR", "F", "GI", "RP", "IG", "R"};
-    }
-}
